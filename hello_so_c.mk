9b6aed86f39549e2faf775e5dde927ad58476c9a
Jan Paesmans <jan.paesmans@gmail.com>
2019-04-25 20:29:57 +0200

    Add runtime library searchpath when linking

--- hello_so_c.mk
+++ hello_so_c.mk
@@ -5,6 +5,6 @@ $(HELLO_SO_C)_OBJS=$(addsuffix .o,$($(HELLO_SO_C)_CSRCS) $($(HELLO_SO_C)_CXXSRCS
 $(HELLO_SO_C)_DEPS=$(addsuffix .d,$($(HELLO_SO_C)_CSRCS) $($(HELLO_SO_C)_CXXSRCS))
 $(HELLO_SO_C)_CFLAGS=$(CFLAGS) -Ihello_so/inc
 $(HELLO_SO_C)_CXXFLAGS=$(CXXFLAGS)
-$(HELLO_SO_C)_LDFLAGS=$(LDFLAGS) -L. -lhello -lstdc++
+$(HELLO_SO_C)_LDFLAGS=$(LDFLAGS) -L. -lhello -lstdc++ -Wl,-rpath=.
 
 $(HELLO_SO_C): libhello.so
--- hello_so_cpp.mk
+++ hello_so_cpp.mk
@@ -5,6 +5,6 @@ $(HELLO_SO_CPP)_OBJS=$(addsuffix .o,$($(HELLO_SO_CPP)_CSRCS) $($(HELLO_SO_CPP)_C
 $(HELLO_SO_CPP)_DEPS=$(addsuffix .d,$($(HELLO_SO_CPP)_CSRCS) $($(HELLO_SO_CPP)_CXXSRCS))
 $(HELLO_SO_CPP)_CFLAGS=$(CFLAGS)
 $(HELLO_SO_CPP)_CXXFLAGS=$(CXXFLAGS) -Ihello_so/inc
-$(HELLO_SO_CPP)_LDFLAGS=$(LDFLAGS) -L. -lhello -lstdc++
+$(HELLO_SO_CPP)_LDFLAGS=$(LDFLAGS) -L. -lhello -lstdc++ -Wl,-rpath=.
 
 $(HELLO_SO_CPP): libhello.so
