b2e00b6132d0ab864c17fec57f1c2d12588310c5
Jan Paesmans <jan.paesmans@gmail.com>
2019-04-26 19:24:26 +0200

    Move sources to appropriate subdirectory

--- hello.a.mk
+++ hello.a.mk
@@ -1,6 +1,6 @@
 HELLO_A=hello.a
-$(HELLO_A)_CSRCS=hello_a.c
-$(HELLO_A)_CXXSRCS=hello_a.cpp
+$(HELLO_A)_CSRCS=hello_a/src/hello_a.c
+$(HELLO_A)_CXXSRCS=hello_a/src/hello_a.cpp
 $(HELLO_A)_OBJS=$(addsuffix .o,$($(HELLO_A)_CSRCS) $($(HELLO_A)_CXXSRCS))
 $(HELLO_A)_DEPS=$(addsuffix .d,$($(HELLO_A)_CSRCS) $($(HELLO_A)_CXXSRCS))
 $(HELLO_A)_CFLAGS=$(CFLAGS)
