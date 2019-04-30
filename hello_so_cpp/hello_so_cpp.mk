00db468911ad342f028daed25aa2d4ac79d5df9a
Jan Paesmans <jan.paesmans@gmail.com>
2019-04-30 20:22:09 +0200

    Move hello_so_cpp to subdir

--- hello_so_cpp/hello_so_cpp.mk
+++ hello_so_cpp/hello_so_cpp.mk
@@ -1,6 +1,6 @@
-HELLO_SO_CPP=hello_so_cpp
+HELLO_SO_CPP=hello_so_cpp/hello_so_cpp
 $(HELLO_SO_CPP)_CSRCS=
-$(HELLO_SO_CPP)_CXXSRCS=hello_main_so.cpp
+$(HELLO_SO_CPP)_CXXSRCS=hello_so_cpp/src/hello_main_so.cpp
 $(HELLO_SO_CPP)_OBJS=$(addsuffix .o,$($(HELLO_SO_CPP)_CSRCS) $($(HELLO_SO_CPP)_CXXSRCS))
 $(HELLO_SO_CPP)_DEPS=$(addsuffix .d,$($(HELLO_SO_CPP)_CSRCS) $($(HELLO_SO_CPP)_CXXSRCS))
 $(HELLO_SO_CPP)_CFLAGS=$(CFLAGS)
@@ -8,3 +8,7 @@ $(HELLO_SO_CPP)_CXXFLAGS=$(CXXFLAGS) -I./hello_so/inc
 $(HELLO_SO_CPP)_LDFLAGS=$(LDFLAGS) -L./hello_so -lhello -lstdc++ -Wl,-rpath=./hello_so
 
 $(HELLO_SO_CPP): hello_so/libhello.so
+
+clean_$(notdir $(HELLO_SO_CPP)):
+	@echo "CLEAN $(HELLO_SO_CPP)"
+	$(Q)rm -f $($(HELLO_SO_CPP)_OBJS) $($(HELLO_SO_CPP)_DEPS)
