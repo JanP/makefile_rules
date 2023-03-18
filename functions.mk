d53314c39866a6586b0a91f798c02c4085afa8fa
Jan Paesmans <jan.paesmans@gmail.com>
2023-03-18 19:33:11 +0100

    Print object size

--- functions.mk
+++ functions.mk
@@ -34,44 +34,49 @@ size = $(if $($(notdir $1)_SIZE),$($(notddir $1)_SIZE),$(SIZE))
 # Function to generate a specific rule for building an application
 define app_rule
 $1: $($(notdir $1)_OBJS)
-	$(call pprintf,"LD",$1)
 	$(Q)$(call ld,$1) -o $1 $($(notdir $1)_OBJS) $($(notdir $1)_LDFLAGS) $(LDFLAGS)
+	@printf "%20s: %s" "LD" "$1"
+	$(Q)$(call size,$1) -t $1 | tail -n 1 | awk '{ printf " (text: %d, data: %d, bss: %d)\n", $$$$1, $$$$2, $$$$3 }'
 endef
 
 # Function to generate a specific rule for building a shared library
 define shared_lib_rule
 $1: $($(notdir $1)_OBJS)
-	$(call pprintf,"LD",$1)
 	$(Q)$(call ld,$1) -o $1 $($(notdir $1)_OBJS) $($(notdir $1)_LDFLAGS) $(LDFLAGS) -shared
+	@printf "%20s: %s" "LD" "$1"
+	$(Q)$(call size,$1) -t $1 | tail -n 1 | awk '{ printf " (text: %d, data: %d, bss: %d)\n", $$$$1, $$$$2, $$$$3 }'
 endef
 
 # Function to generate a specific rule for building an archive
 define archive_rule
 $1: $($(notdir $1)_OBJS)
-	$(call pprintf,"AR",$1)
 	$(Q)$(call ar,$1) $(ARFLAGS) $1 $($(notdir $1)_OBJS)
+	@printf "%20s: %s" "AR" "$1"
+	$(Q)$(call size,$1) -t $1 | tail -n 1 | awk '{ printf " (text: %d, data: %d, bss: %d)\n", $$$$1, $$$$2, $$$$3 }'
 endef
 
 # Function to generate a specific rule to create each object file and the
 # dependencies for regenerating the object file based on the C source
 define c_obj_rule
 $1.$2.o $1.$2.d: $1
-	$(call pprintf,"C",$1)
 	$(Q)$(call cc,$2) -c -o $1.$2.o $1 -MT $1.$2.o -MMD -MP -MF $1.$2.d $($(notdir $2)_CFLAGS) $(CFLAGS) $3
+	@printf "%20s: %s" "C" "$1"
+	$(Q)$(call size,$2) -t $1.$2.o | tail -n 1 | awk '{ printf " (text: %d, data: %d, bss: %d)\n", $$$$1, $$$$2, $$$$3 }'
 endef
 
 # FUnction to generate a specific rule to create each object file and the
 # dependencies for regenerating the object file based on the C++ source
 define cxx_obj_rule
 $1.$2.o $1.$2.d: $1
-	$(call pprintf,"C++",$1)
 	$(Q)$(call cxx,$2) -c -o $1.$2.o $1 -MT $1.$2.o -MMD -MP -MF $1.$2.d $($(notdir $2)_CXXFLAGS) $(CXXFLAGS) $3
+	@printf "%20s: %s" "C++" "$1"
+	$(Q)$(call size,$2) -t $1.$2.o | tail -n 1 | awk '{ printf " (text: %d, data: %d, bss: %d)\n", $$$$1, $$$$2, $$$$3 }'
 endef
 
 # Functio nto generate a specific rule to clean up generated objects and
 # dependencies for a specified build target
 define clean_rule
 clean_$1:
-	$(call pprintf,"CLEAN",$1)
+	@printf "%20s: %s\n" "CLEAN" "$1"
 	$(Q)rm -f $($1_OBJS) $($1_DEPS)
 endef
